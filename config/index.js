51b26ac61cf5cc9e4be1e8fa1300904a5a1bfc19
Khrystenko <volodymyr.khrystenko@atos.net>
2018-03-05 14:24:52 +0100

    Working Mongodb

---
+++ config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "mongodb":"mongodb://192.168.83.129:27017"
+};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  req.app.get('db_blog').collection('hello_message').findOne({}, function(err, doc) {
+    if(err) throw err;
+    res.render('index', doc);
+  });
 });
 
 module.exports = router;
